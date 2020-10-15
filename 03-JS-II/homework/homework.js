4bfbd0c210763673b11a69ae144244a2141e18dc
franco <franco_allemandi@hotmail.com>
2020-10-15 19:51:45 -0300

    tarea HTML

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,6 +5,8 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+  if(x>y){return x;}
+    else{return y;}
 }
 
 function saludo(idioma) {
@@ -14,18 +16,27 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  if(idioma==='aleman'){return 'Guten Tag!';}
+  else if(idioma==='mandarin'){return 'Ni Hao!';}
+  else if(idioma==='ingles'){return 'Hello!';}
+  else {return 'Hola!';}
 }
 
 function esDiezOCinco(numero) {
     // Devuelve "true" si "numero" es 10 o 5
     // De lo contrario, devuelve "false"
     // Tu código:
+    if (numero===5){return true;}
+    else if(numero===10){return true;}
+    else{return false};
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  if(numero<50 && numero>20){return true;}
+  return false;
 }
 
 function esEntero(numero) {
@@ -36,6 +47,9 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+  var redond=Math.floor(numero);
+  if(numero===redond){return true;}
+  return false;
 }
 
 function fizzBuzz(numero) {
@@ -43,6 +57,12 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+  if(numero%3===0){
+    if (numero%5===0){return 'fizzbuzz';}
+    return 'fizz';
+  }
+  else if (numero%5===0){return 'buzz';}
+  return numero;
 }
 
 function esPrimo(numero) {
@@ -51,6 +71,13 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+  if (numero===0){return false;}
+  if (numero===1){return false;}
+  for(var i=2;i<numero;i++){
+    result=numero%i;
+    if(result===0){return false;}
+  }
+  return true;
 }
 
 
